322f0473264472d11f08aec17e51a4841c858f41
junho.han <junho.han@ebridge-world.com>
2021-12-06 03:18:10 +0900

    종료 기능

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { componentsPlugin } from "bootstrap-vue";
 import "./style.css";
 
 const modal = document.querySelector(".modal");
@@ -18,7 +19,7 @@ const completedTodos = document.querySelector("#completed-todos");
 let globalInputTitle;
 let globalInputLimitTime;
 let todoListData = [];
-
+let timerPool = [];
 window.onload = () => {
   checkInputTitle.focus();
 };
@@ -34,7 +35,6 @@ checkInputTitle.addEventListener("keyup", function() {
 checkInputLimitTime.addEventListener("keyup", function() {
   globalInputLimitTime = this.value;
 });
-
 sortParent.addEventListener("click", function(event) {
   console.log(event.target);
   sortByRegistTime.classList.toggle("checked");
@@ -46,7 +46,7 @@ sortByRegistTime.addEventListener("click", function() {
     return a.registerTime - b.registerTime;
   });
   todoListData.forEach((item) => {
-    var li = document.createElement("li");
+    let li = document.createElement("li");
     let titleSpan = document.createElement("span");
     titleSpan.innerHTML = item.title;
     li.appendChild(titleSpan);
@@ -54,7 +54,7 @@ sortByRegistTime.addEventListener("click", function() {
     li.style.listStyle = "none";
     ul.appendChild(li);
 
-    var timerSpan = document.createElement("span");
+    let timerSpan = document.createElement("span");
     timerSpan.setAttribute("id", "stopWatchDisplay");
     timerSpan.classList.add("timerDisplay");
     timerSpan.innerHTML = item.limitTime;
@@ -70,6 +70,7 @@ inputSumbitBtn.onclick = function() {
   const inputLimitTime = globalInputLimitTime;
   let time = inputLimitTime;
   let hasDone = inputLimitTime > 0 ? false : true;
+  let interval;
 
   const validation = Boolean(inputTitle && inputLimitTime);
   if (!validation) {
@@ -98,7 +99,8 @@ inputSumbitBtn.onclick = function() {
     checkInputTitle.focus();
   }
   function render() {
-    var li = document.createElement("li");
+    let li = document.createElement("li");
+    li.setAttribute("id", `task-${Date.now()}`);
     let titleSpan = document.createElement("span");
     titleSpan.innerHTML = inputTitle;
     li.appendChild(titleSpan);
@@ -106,7 +108,7 @@ inputSumbitBtn.onclick = function() {
     li.style.listStyle = "none";
     ul.appendChild(li);
 
-    var timerSpan = document.createElement("span");
+    let timerSpan = document.createElement("span");
     timerSpan.setAttribute("id", `stopWatchDisplay-${Date.now()}`);
     timerSpan.classList.add("timerDisplay");
     timerSpan.innerHTML = inputLimitTime;
@@ -121,7 +123,16 @@ inputSumbitBtn.onclick = function() {
     completeBtn.setAttribute("id", "complete-btn");
     completeBtn.innerText = "종료";
     completeBtn.addEventListener("click", (event) => {
-      makeComplete(event);
+      let children = event.target.parentNode.parentNode.childNodes;
+      let index = -1;
+      for (let i = 0; i < children.length; i++) {
+        if (children[i].id === event.target.parentNode.id) {
+          index = i;
+          break;
+        }
+      }
+      todoListData.splice(index, 1);
+      addCompleteList(event.target, inputTitle, inputLimitTime, interval);
     });
 
     li.appendChild(completeBtn);
@@ -129,39 +140,25 @@ inputSumbitBtn.onclick = function() {
   }
 
   function increment(timerSpan) {
-    let interval;
     if (!hasDone) {
       interval = setTimeout(() => {
         time--;
         if (time == 0) {
           displayModal();
-          let target = timerSpan.parentNode;
-          target.parentNode.removeChild(target);
-
-          var li = document.createElement("li");
-          let completeTitle = document.createElement("span");
-          completeTitle.innerHTML = inputTitle;
-          li.appendChild(completeTitle);
-          li.classList.add("task");
-          li.style.listStyle = "none";
-          completedTodos.appendChild(li);
-
-          var completeLimitTime = document.createElement("span");
-          completeLimitTime.innerHTML = inputLimitTime;
-          li.appendChild(completeLimitTime);
-
+          addCompleteList(timerSpan, inputTitle, inputLimitTime);
           hasDone = true;
         }
         timerSpan.innerHTML = time;
         increment(timerSpan);
       }, 1000);
+      timerPool.push(interval);
     }
   }
 };
 removeCheckedBtn.addEventListener("click", function() {
-  var body = document.getElementById("todos");
-  var chkbox = document.querySelectorAll("#todos .task #check");
-  for (var i in chkbox) {
+  let body = document.getElementById("todos");
+  let chkbox = document.querySelectorAll("#todos .task #check");
+  for (let i in chkbox) {
     if (chkbox[i].nodeType == 1 && chkbox[i].checked == true) {
       body.removeChild(chkbox[i].parentNode);
     }
@@ -169,13 +166,39 @@ removeCheckedBtn.addEventListener("click", function() {
 });
 
 removeAll.onclick = function() {
-  let allTodos = document.querySelectorAll(".todo");
-  for (let i = 0; i < todoListData.length; i++) {
-    allTodos[i].remove();
+  let body = document.getElementById("todos");
+  todoListData.forEach((item) => {
+    makeList(item.title, item.limitTime);
+  });
+  timerPool.forEach((timer) => {
+    clearTimeout(timer);
+  });
+  while (body.hasChildNodes()) {
+    body.removeChild(body.firstChild);
   }
   todoListData = [];
+  timerPool = [];
 };
 
+function addCompleteList(node, inputTitle, inputLimitTime, interval) {
+  clearTimeout(interval);
+  let target = node.parentNode;
+  target.parentNode.removeChild(target);
+  makeList(inputTitle, inputLimitTime);
+}
+function makeList(inputTitle, inputLimitTime) {
+  let li = document.createElement("li");
+  let completeTitle = document.createElement("span");
+  completeTitle.innerHTML = inputTitle;
+  li.appendChild(completeTitle);
+  li.classList.add("task");
+  li.style.listStyle = "none";
+  completedTodos.appendChild(li);
+
+  let completeLimitTime = document.createElement("span");
+  completeLimitTime.innerHTML = inputLimitTime;
+  li.appendChild(completeLimitTime);
+}
 //check박스를 클릭 할때 마다 스타일 변화 및 삭제기능 추가
 function todoClickEvent(target, data) {
   //this 지정
@@ -202,7 +225,7 @@ function todoClickEvent(target, data) {
     });
     // 수정
     target[i].childNodes[7].addEventListener("click", function() {
-      var prompt = window.prompt("수정할 내용을 입력해주세요");
+      let prompt = window.prompt("수정할 내용을 입력해주세요");
       if (prompt.length > 0) {
         this.parentNode.childNodes[3].innerHTML = prompt;
         data[i] = prompt;
@@ -210,13 +233,8 @@ function todoClickEvent(target, data) {
     });
   }
 }
-function makeComplete(event) {
-  const parent = event.target.parentNode.parentNode;
-  parent.removeChild(event.target.parentNode);
-}
-
 function resetChild() {
-  var cell = document.getElementById("todos");
+  let cell = document.getElementById("todos");
   while (cell.hasChildNodes()) {
     cell.removeChild(cell.firstChild);
   }
